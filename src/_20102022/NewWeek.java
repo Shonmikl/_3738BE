f022b377d381afc0f2ae12fabf4d20ef945933b4
shonmikl <mikhailegorov676@gmail.com>
2022-10-20 21:21:43 +0300

    enum

---
+++ src/_20102022/NewWeek.java
@@ -0,0 +1,11 @@
+package _20102022;
+
+public enum NewWeek {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY;
+}
--- src/_20102022/Week.java
+++ src/_20102022/Week.java
@@ -1,11 +1,42 @@
 package _20102022;
 
 public enum Week {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY
+    MONDAY("bad"),
+    TUESDAY("bad"),
+    WEDNESDAY("so-so"),
+    THURSDAY("so-so"),
+    FRIDAY("good"),
+    SATURDAY("nice"),
+    SUNDAY("good");
+
+    private String mood;
+
+    Week(String mood) {
+        this.mood = mood;
+    }
+
+    public String getMood() {
+        return mood;
+    }
+}
+
+class Today {
+    Week week;
+
+    public Today(Week week) {
+        this.week = week;
+    }
+
+    void getDay() {
+        switch (week) {
+            case MONDAY,
+                    TUESDAY,
+                    THURSDAY,
+                    FRIDAY,
+                    WEDNESDAY -> System.out.println("Study JAVA");
+            case SATURDAY,
+                    SUNDAY -> System.out.println("RELAX");
+        }
+        System.out.println(week.getMood());
+    }
 }
