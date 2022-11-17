20ce5f1787f10a44988e4c04526358ad4a6b6995
shonmikl <mikhailegorov676@gmail.com>
2022-11-17 23:26:31 +0300

    Bit

--- src/_17_11_2022/Generator.java
+++ src/_17_11_2022/Generator.java
@@ -2,7 +2,21 @@ package _17_11_2022;
 
 import lombok.experimental.UtilityClass;
 
+import java.util.*;
+
 @UtilityClass
 public class Generator {
+    List<String> list = new LinkedList<>();
+    Set<String> set = new HashSet<>();
 
+    public void addToSetAndList(String s) {
+        list.add(s);
+        set.add(s);
+    }
+
+    public static void main(String[] args) {
+        char a = 'A';
+        a++;
+        System.out.println(a);
+    }
 }
