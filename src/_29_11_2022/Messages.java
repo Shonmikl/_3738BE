bbc8100df156b8288afc375aa2d07f591dd2671d
shonmikl <mikhailegorov676@gmail.com>
2022-11-29 23:28:59 +0300

    String

---
+++ src/_29_11_2022/Messages.java
@@ -0,0 +1,9 @@
+package _29_11_2022;
+
+public class Messages {
+    private final static String LENGTH = "The length of this array have to be over than 2";
+
+    public static String getLengthMessages() {
+        return LENGTH;
+    }
+}
