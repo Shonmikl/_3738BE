23279c5afbb7ffe1033337d5007a6a4b0c355944
IrinaNaumenko <irina1280808@gmail.com>
2022-12-01 18:48:30 +0100

    Homework next task

---
+++ src/homework/project/datebase/Date_Base.java
@@ -0,0 +1,20 @@
+package homework.project.datebase;
+
+import homework.project.model.LivingRoom;
+import homework.project.model.Office;
+import homework.project.model.Room;
+import homework.project.model.Shop;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.TreeMap;
+
+public class Date_Base {
+    private List<Shop> roomShop= new LinkedList<>();
+    private List<Office> roomOffice=new LinkedList<>();
+    private List<LivingRoom> livingRooms=new LinkedList<>();
+
+    private TreeMap<Room,Integer> testMap = new TreeMap<>();
+
+
+}
