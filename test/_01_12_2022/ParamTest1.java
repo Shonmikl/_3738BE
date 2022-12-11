9d3969b3fbf35b7d0f13a852b0ddf369e851d33b
Lelik <kentaskis.l@gmail.com>
2022-12-11 09:39:33 +0100

    tests

--- test/_01_12_2022/ParamTest1.java
+++ test/_01_12_2022/ParamTest1.java
@@ -1,10 +1,5 @@
 package _01_12_2022;
 
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
 public class ParamTest1 {
 
 //    @Test
@@ -14,9 +9,9 @@ public class ParamTest1 {
 //        Assertions.assertTrue("8888".length() > 3);
 //    }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"888888", "88888", "8888"})
-    static void testLength(String args) {
-        Assertions.assertTrue(args.length() > 3);
-    }
+//    @ParameterizedTest
+//    @ValueSource(strings = {"888888", "88888", "8888"})
+//    static void testLength(String args) {
+//        Assertions.assertTrue(args.length() > 3);
+//    }
 }
--- test/alexey_lavrov/project/util/GeneratorTest.java
+++ test/alexey_lavrov/project/util/GeneratorTest.java
@@ -1,9 +1,12 @@
 package alexey_lavrov.project.util;
 
+import alexey_lavrov.project.models.AdultWorkerBee;
 import alexey_lavrov.project.models.Bee;
-import alexey_lavrov.project.models.Queen;
 import alexey_lavrov.project.models.WorkerBee;
 import org.junit.jupiter.api.Test;
+
+
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,8 +14,13 @@ class GeneratorTest {
 
     @Test
     void makeBee() throws Exception {
-        Bee bee = Generator.makeBee(Queen.class);
-        assertTrue( bee instanceof Queen);
+        Bee bee = Generator.makeBee(AdultWorkerBee.class);
+        assertTrue( bee instanceof AdultWorkerBee);
+        assertTrue(bee.getName() instanceof String);
+        assertTrue(bee.getSize() instanceof Float);
+        assertTrue(bee.getBirthday() instanceof Date);
+        assertTrue(bee.getId() instanceof Integer);
+        assertTrue(bee.getName() instanceof String);
         Bee bee2 = Generator.makeBee(WorkerBee.class);
         assertTrue( bee2 instanceof WorkerBee);
     }
