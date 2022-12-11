26bffb6a61f777643e847b6a2ca7c1f480809c88
Lelik <kentaskis.l@gmail.com>
2022-12-11 10:08:45 +0100

    tests

--- test/_01_12_2022/ParamTest2.java
+++ test/_01_12_2022/ParamTest2.java
@@ -1,23 +1,23 @@
 package _01_12_2022;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class ParamTest2 {
 
-//    @ParameterizedTest
-//    @CsvSource(value = {"OIUYT,oiuyt", "MNBVC,mnbvc", "'',''"})
-//    void testLowerCase(String arg, String lowered) {
-//        Assertions.assertEquals(lowered, arg.toLowerCase());
-//    }
+    @ParameterizedTest
+    @CsvSource(value = {"OIUYT,oiuyt", "MNBVC,mnbvc", "'',''"})
+    void testLowerCase(String arg, String lowered) {
+        Assertions.assertEquals(lowered, arg.toLowerCase());
+    }
 //
 //    @ParameterizedTest
 //    @CsvFileSource(resources = "2222.csv")
 //    void testLowerCaseFile(String arg, String lowered) {
 //        Assertions.assertEquals(lowered, arg.toLowerCase());
 //    }
-
     @Disabled("Because....")
     void testDisabled() {
         System.out.println("!!!!!");
