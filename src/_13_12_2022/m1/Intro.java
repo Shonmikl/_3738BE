9c4b020229e8d67ccc5b5bf0fd35d1f6d81bab0c
shonmikl <mikhailegorov676@gmail.com>
2022-12-15 23:44:43 +0300

    Thread

--- src/_13_12_2022/m1/Intro.java
+++ src/_13_12_2022/m1/Intro.java
@@ -1,11 +1,18 @@
 package _13_12_2022.m1;
 
+import java.util.Arrays;
+
 public class Intro {
     public static void main(String[] args) {
-        System.out.println("Hi");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
-        System.out.println("Buy");
+//        System.out.println("Hi");
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(i);
+//        }
+//        System.out.println("Buy");
+        int a = 0;
+       // System.out.println(a);
+        Integer a1[] = new Integer[5];
+        System.out.println(Arrays.toString(a1));
+
     }
 }
