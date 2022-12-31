9fd76c127f3ab02c4ef1afb56f27fe2e9e93bf6d
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2022-12-31 16:16:40 +0100

    HW_Task1.2_Task2.5

--- src/HW_2023/HW2_FibonacciNumber.java
+++ src/HW_2023/HW2_FibonacciNumber.java
@@ -1,6 +1,7 @@
 package HW_2023;
 
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class HW2_FibonacciNumber {
     public static void main(String[] args) {
@@ -50,5 +51,25 @@ public class HW2_FibonacciNumber {
         }
     }
 
+    public static class fibStream {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int index, n0, n1, fib = 0;
+            System.out.println("Enter the integer index number of the fibonacci series from 1 to 92: ");
+            index = sc.nextInt();
+            System.out.println("index = " + index);
 
+            Stream.iterate(new int[]{0, 1}, arr -> new int[]{arr[1], arr[0] + arr[1]})
+
+                    //Задаём лимит значений:
+                    .limit(index+1)
+
+                    //Отбираем по первому элементу каждого массива:
+                    .map(y -> y[0])
+
+                    //Выводим в консоль:
+                    .forEach(x -> System.out.print(x+", "));
+        }
+
+    }
 }
