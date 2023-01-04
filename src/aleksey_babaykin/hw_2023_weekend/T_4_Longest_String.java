51ba603e1763dd9d9cd89cc820d815ce709d9f7c
oleksiibabaikin <a.babaykin@gmail.com>
2023-01-04 10:08:41 +0200

    HomeWork 2023 4

--- src/aleksey_babaykin/hw_2023_weekend/T_4_Longest_String.java
+++ src/aleksey_babaykin/hw_2023_weekend/T_4_Longest_String.java
@@ -1,16 +1,23 @@
 package aleksey_babaykin.hw_2023_weekend;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.stream.Stream;
-
 public class T_4_Longest_String {
     public static void main(String[] args) {
 
-        String[] oldString = {"we", "wee", "qwer", "oooo"};
+        String[] oldString1 = {"we", "wee", "qwer", "oooo"};
+        String[] oldString2 = {"we", "iehe","wee", "qwer", "oooo"};
+        getLongestStrings(oldString1);
+        getLongestStrings(oldString2);
 
-        Stream.of(oldString)
+    }
+    static void getLongestStrings(String[] inStringArray) {
+        System.out.printf("\nStart array Strings: %s \n", Arrays.toString(inStringArray));
+
+        Stream.of(inStringArray)
                 .filter(s ->
-                        s.length() == Stream.of(oldString)
+                        s.length() == Stream.of(inStringArray)
                                 .max(Comparator.comparing(String::length))
                                 .get().length())
                 .forEach(System.out::println);
