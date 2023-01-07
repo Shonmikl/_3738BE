09d8ee37611ab6318d4f902be49517fd8b17a52d
oleksiibabaikin <a.babaykin@gmail.com>
2023-01-07 18:27:56 +0200

    HomeWork 2023. Task #7, Update: Upper all symbols, not only first

--- src/aleksey_babaykin/hw_2023_weekend/T_7_Student.java
+++ src/aleksey_babaykin/hw_2023_weekend/T_7_Student.java
@@ -16,29 +16,37 @@ public class T_7_Student {
         initList();
         changeStudent(list);
     }
+
     static void changeStudent(List<Student> inList) {
         inList.stream()
                 .map(item -> {
-                    item.setName(item.getName().substring(0, 1).toUpperCase() + item.getName().substring(1));
+//                    item.setName(item.getName().substring(0, 1).toUpperCase() + item.getName().substring(1)); /// Это если нужно слелать заглавной только первую букву ///
+                    item.setName(item.getName().toUpperCase());
+                    item.setSurname(item.getSurname().toUpperCase().toUpperCase());
                     return item;
                 })
-                .collect(Collectors.groupingBy(Student::getCourse , Collectors.mapping(Student::getName, Collectors.joining(" / "))))
+                .collect(Collectors.groupingBy(Student::getCourse,
+                        Collectors.mapping((item -> String.format("%s %s", item.getSurname(), item.getName())),
+                                Collectors.joining(" / "))))
                 .entrySet()
-                .forEach(item -> System.out.println("Course : " + item.getKey()+"; Students name : "+ item.getValue()));
+                .forEach(item -> System.out.println("Course : " + item.getKey() + "; Students name : " + item.getValue()));
     }
+
     public static void initList() {
-        list.add(new Student("vasiliy", 'm', 30, 5, 4.0));
-        list.add(new Student("ivan", 'm', 31, 5, 4.1));
-        list.add(new Student("aleksey", 'm', 32, 7, 4.2));
-        list.add(new Student("nataliya", 'w', 33, 7, 4.3));
+        list.add(new Student("vasiliy", "vasiliev", 'm', 30, 5, 4.0));
+        list.add(new Student("ivan", "ivanov", 'm', 31, 5, 4.1));
+        list.add(new Student("aleksey", "alekseev", 'm', 32, 7, 4.2));
+        list.add(new Student("nataliya", "natalieva", 'w', 33, 7, 4.3));
     }
 }
+
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
 class Student {
     private String name;
+    private String surname;
     private char sex;
     private int age;
     private int course;
